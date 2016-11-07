06e1ec0715e19bce719647e4c5eafea424375875
Stephen D. Hack <stephen.hack@gmail.com>
2016-11-07 13:55:35 -0500

    Nov7

--- BetterDoctorsApp/client/assets/controllers/doctorController.js
+++ BetterDoctorsApp/client/assets/controllers/doctorController.js
@@ -1,19 +1,34 @@
-app.controller('doctorController', ['$scope', '$window', '$http', 'userFactory', '$cookies', '$location', function($scope, $window, $http, userFactory, $cookies, $location){
-     $scope.getDoctors = function () {
-            $window.navigator.geolocation.getCurrentPosition(function(position) {
-                $scope.$apply(function() {
-                    $scope.latitude = position.coords.latitude;
-                    $scope.longitude = position.coords.longitude;
-                    $scope.accuracy = position.coords.accuracy;
-                    $scope.url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=' + $scope.latitude.toString() + '%2C-' + $scope.longitude.toString()
-                    console.log($scope.url)
-                });
-                }, function(error) {
-                    alert(error);
+app.controller('doctorController', ['$scope', '$window', '$http', 'doctorFactory', '$cookies', '$location', function($scope, $window, $http, doctorFactory, $cookies, $location){
+      $scope.insurances = [];
+      //Use Navigator to get Latitude and Longitude of User
+     $scope.getLatLong = function () {
+        $window.navigator.geolocation.getCurrentPosition(function(position) {
+            $scope.$apply(function() {
+                $scope.latitude = position.coords.latitude;
+                $scope.longitude = position.coords.longitude;
+                $scope.accuracy = position.coords.accuracy;
             });
-        }
+            }, function(error) {
+                alert(error);
+        });
+      }
+    $scope.getLatLong();
+    $scope.getInsurance = function () {
+      insurance_url = 'https://api.betterdoctor.com/2016-03-01/insurances?user_key=042d852d3e7416f52f932e01afaee003'
+      if ($scope.insurances != [])
+        $http.get(insurance_url).then(function(res) {
+          console.log('insurances', res)
+          console.log(res.data.data)
+        })
+    }
+    $scope.getInsurance();
+
     $scope.findDoctors = function () {
-      console.log("Find Doctors", $scope.filter)
+      doctorFactory.findDoctors($scope, function (res) {
+        console.log('res.data', res.data)
+        console.log('res.data.data', res.data.data)
+        console.log('Total Doctors', res.data.meta.total)
+      })
     }
     
 
--- BetterDoctorsApp/client/assets/factories/doctorFactory.js
+++ BetterDoctorsApp/client/assets/factories/doctorFactory.js
@@ -1,18 +1,48 @@
 app.factory('doctorFactory', ['$http', function($http) {
-
   function doctorFactory(){
- 	this.login = function(data, callback) {
- 		console.log(data);
- 		$http.post('/login', data).then(
- 			function(returned_data) {
- 				callback(returned_data);
-        		console.log('Returned Data', returned_data.data)
- 			});
+ 	this.findDoctors = function(data, callback) {
+ 		//Set up HTTP Orject to make request
+ 		var xhttp = new XMLHttpRequest();
+ 		//Prepare variables to create URL which will be used for API call
+ 		var start = 'https://api.betterdoctor.com/2016-03-01/doctors?'
+ 		//API key for Better Doctors
+ 		var user_key = 'user_key=042d852d3e7416f52f932e01afaee003'
+ 		if (data.filter.first_name) {
+ 			start += 'first_name=' + data.filter.first_name + '&'
  		}
- 	this.getLocation = function(callback) {
- 		
+ 		if (data.filter.last_name) {
+ 			start += 'last_name=' + data.filter.last_name + '&'
+ 		}
+ 		if (data.filter.query) {
+ 			start += 'query=' + data.filter.query + '&'
+ 		}
+ 		if (data.filter.insurance) {
+ 			start += 'insurance_uid=' + data.filter.insurance + '&'
+ 		}
+ 		if (data.latitude) {
+ 			start += 'location=' + data.latitude.toString() + '%2C'
+ 		}
+ 		if (data.longitude) {
+ 			start += data.longitude.toString() + '%2C25&'
+ 		}
+ 		if (data.filter.gender) {
+ 			start += 'gender=' + data.filter.gender + '&'
+ 		}
+ 		if (data.filter.sort) {
+ 			start += 'sort=' + data.filter.sort + '&'
+ 		}
+ 		if (data.filter.fields) {
+ 			start += 'fields=' + data.filter.fields + '&skip=0&'
+ 		}
+ 		if (data.filter.limit) {
+ 			start += 'limit=' + data.filter.limit + '&'
+ 		}
+ 		start += user_key
+ 		//Make an API call with URL 
+ 		$http.get(start).then(function(res) {
+ 			callback(res)
+ 		})
  	}
-
     }
     return new doctorFactory();
 }]);
