ef9b75e0af56b691b02bde39142dab08c6646156
Stephen D. Hack <stephen.hack@gmail.com>
2016-11-14 14:30:57 -0500

    Nov14

--- BetterDoctorsApp/client/assets/controllers/doctorController.js
+++ BetterDoctorsApp/client/assets/controllers/doctorController.js
@@ -73,4 +73,36 @@ app.controller('doctorController', ['$scope', '$window', '$http', 'doctorFactory
       };
       $scope.doctor = dataToPass
     }
+    // function insuranceController ($timeout, $q, $log) {
+    //   var self = this;
+    //   self.simulateQuery = false;
+    //   self.isDisabled    = false;
+    //   self.insurances        = $scope.insurances;
+    //   self.querySearch   = querySearch;
+    //   self.selectedItemChange = selectedItemChange;
+    //   self.searchTextChange   = searchTextChange;
+    // }
+
+
+    // function querySearch (query) {
+    //   var results = query ? $scope.insurances.filter( createFilterFor(query) ) : $scope.insurances;
+    //   var deferred = $q.defer();
+    //   $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+    //   return deferred.promise;
+    // }
+    // function searchTextChange(text) {
+    //   $log.info('Text changed to ' + text);
+    // }
+
+    // function selectedItemChange(item) {
+    //   $log.info('Item changed to ' + JSON.stringify(item));
+    // }
+
+    // function createFilterFor(query) {
+    //   var lowercaseQuery = angular.lowercase(query);
+    //   return function filterFn(insurance) {
+    //     return (insurance.value.indexOf(lowercaseQuery) === 0);
+    //   };
+    // }
+
 }]);
