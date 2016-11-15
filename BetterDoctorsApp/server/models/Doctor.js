1a67d27047520fe86a2eab5fcab2bbd5ad82ffd9
Stephen D. Hack <stephen.hack@gmail.com>
2016-11-15 13:55:06 -0500

    Nov15

--- BetterDoctorsApp/server/models/Doctor.js
+++ BetterDoctorsApp/server/models/Doctor.js
@@ -2,10 +2,20 @@ var mongoose = require('mongoose');
 
 var DoctorSchema = new mongoose.Schema({
 	doctor_uid: {
-		
-	name: {
 		type: String,
 	},
+	first_name: {
+		type: String,
+	},
+	middle_name: {
+		type: String,
+	},
+	last_name: {
+		type: String,
+	},
+	practices: {
+		type: Array,
+	}
 	category: {
 		type: String,type: String,
 	},
